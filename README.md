ba95980ab1382a7bf71720f62ca1cf1c75398b5c
unknown <stuohino@UK-LDN-DT287.office.ad.netbenefit.com>
2014-07-25 11:20:23 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+win-setup
+=========
+
+Install Chocolatey and tools. 
+
+Launch in a command prompt as and Administrator (cmd -> Run As Administrator), type powershell, hit enter and run the script.
---
+++ install_dev_tools.ps1
@@ -0,0 +1,21 @@
+﻿set ExecutionPolicy Remote-Signed
+
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+cinst GoogleChrome
+cinst notepadplusplus
+cinst ransack
+cinst git
+cinst tortoisesvn
+cinst mremoteng
+cinst putty
+cinst soapui
+cinst nodejs
+cinst consolez
+cinst pstools
+cinst lockhunter
+cinst javaruntime
+cinst ActivePerl
+cinst jenkins
+cinst curl
+cinst bind-toolsonly
